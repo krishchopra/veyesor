7412b9129bc8928f78da7965b7ed66f8524e6264
Krish Chopra <krish.chopra23@gmail.com>
2025-01-26 01:55:21 -0500

    edits

--- webapp/app/components/VideoPlayer360.tsx
+++ webapp/app/components/VideoPlayer360.tsx
@@ -2,15 +2,18 @@
 
 import dynamic from "next/dynamic";
 import { useRef } from "react";
+import type ReactPlayer from "react-player";
 
-const ReactPlayer = dynamic(() => import("react-player"), { ssr: false });
+const ReactPlayerComponent = dynamic(() => import("react-player"), {
+	ssr: false,
+});
 
 export default function VideoPlayer360() {
-	const playerRef = useRef<any>(null);
+	const playerRef = useRef<ReactPlayer>(null);
 
 	return (
 		<div className="relative w-full h-screen">
-			<ReactPlayer
+			<ReactPlayerComponent
 				ref={playerRef}
 				url="/sample360.mp4"
 				width="100%"
